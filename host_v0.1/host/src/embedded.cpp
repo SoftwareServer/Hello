52e6239c262c2ba39ed2f049463e868e8f341c49
SoftwareServer <yhlk2018@163.com>
2020-04-27 14:02:58 +0800

    模拟“宿主”程序v0.2
    1、增加对数据意义说明字典（自定义扩展），作为用户处理和保存数据的标记
    2、增加交互式数据查询接口

--- host_v0.1/host/src/embedded.cpp
+++ host_v0.1/host/src/embedded.cpp
@@ -10,6 +10,7 @@
 #include<net/if.h>
 #include<mutex>
 #include<vector>
+#include<map>
 using namespace std;
 struct host{
     string id;
@@ -18,8 +19,19 @@ struct host{
     string pid;
     string type;
     string value;
+    int comm_id;
+    string comments;
 };
-vector<host> host_data;
+map<int,string> dic_comments;
+void init_dic(){
+    dic_comments[0]="robot_num";
+    dic_comments[1]="packets";
+    dic_comments[2]="data_lenth";
+    dic_comments[3]="speed";
+    dic_comments[4]="signal";
+    dic_comments[5]="error_rate";
+}
+vector<host> host_data;  //similar to database
 mutex lock_s;
 int get_local_ip(const char *eth_inf,char *ip){
     int sd;
@@ -53,9 +65,9 @@ void timeToDate(struct timeval tv,char *time){
     localtime_r(&tv.tv_sec,&tv_temp);
     snprintf(time,25,"%04d-%02d-%02d %02d:%02d:%02d.%03ld",tv_temp.tm_year+1900,tv_temp.tm_mon+1,tv_temp.tm_mday,tv_temp.tm_hour,tv_temp.tm_min,tv_temp.tm_sec,tv.tv_usec/1000);
 }
-void process(int n){
+void process(int n,int comm){
     char ip_num[20] = "";
-    get_local_ip("eth0",ip_num);
+    get_local_ip("eth0",ip_num);   //network card name
     struct timeval nowTus;
     gettimeofday(&nowTus,NULL);
     char time[25]="";
@@ -67,11 +79,13 @@ void process(int n){
     temp.pid=to_string(getpid());
     temp.type="int";
     temp.value=to_string(n);
+    temp.comm_id=comm;
+    temp.comments=dic_comments[comm];
     lock_s.lock();
-    host_data.push_back(temp);
+    host_data.push_back(temp);    //insert to the struct
     lock_s.unlock();
 }
-void process(float n){
+void process(float n,int comm){
     char ip_num[20] = "";
     get_local_ip("eth0",ip_num);
     struct timeval nowTus;
@@ -85,11 +99,13 @@ void process(float n){
     temp.pid=to_string(getpid());
     temp.type="float";
     temp.value=to_string(n);
+    temp.comm_id=comm;
+    temp.comments=dic_comments[comm];
     lock_s.lock();
     host_data.push_back(temp);
     lock_s.unlock();
 }
-void process(double n){
+/*void process(double n){
     char ip_num[20] = "";
     get_local_ip("eth0",ip_num);
     struct timeval nowTus;
@@ -125,14 +141,246 @@ void process(string str){
     lock_s.lock();
     host_data.push_back(temp);
     lock_s.unlock();
+}*/
+void cinString(string& data){
+    data.clear();
+    cin>>data;
+    std::cin.clear();
+    std::cin.ignore(INT_MAX, '\n');
 }
-void show(){
-    if(!host_data.empty()){
-	lock_s.lock();
-	cout<<"No\t"<<"time"<<setw(13)<<"\t"<<"ip"<<setw(7)<<"\t"<<"pid\t"<<"type\t"<<"value"<<endl;
-	for(int i=0;i<host_data.size();i++){
-	    cout<<host_data[i].id<<"\t"<<host_data[i].time<<"\t"<<host_data[i].ip<<"\t"<<host_data[i].pid<<"\t"<<host_data[i].type<<"\t"<<host_data[i].value<<endl;
-	}
-	lock_s.unlock();
+
+bool cinInt(int& data){  //judge the input str whether it is a digit
+    std::string tmp = "";
+    cinString(tmp);
+
+    int i = 0;
+    while(i < tmp.size() && tmp[i] != '\0' && isdigit(tmp[i])){
+         i++;
     }
+
+    if (i<tmp.size()) {
+        cout<<"invalid input!"<<endl;
+        return false;
+    } else {
+        data = atoi(tmp.c_str());
+        return true;
+    }
+}
+void host_show(){  //query API
+    int sel = -1;
+    while(sel != 0){
+        cout<<"#########################################################"<<endl;
+        cout<<"# 1. query all data \t\t 2. query of robot_num "<<endl;
+        cout<<"# 3. query of packets \t\t 4. query of data_lenth "<<endl;
+        cout<<"# 5. query of speed \t\t 6. query of signal "<<endl;
+        cout<<"# 7. query of error_rate\t 8. clear the history "<<endl;
+        cout<<"# 0. exit "<<endl;
+        cout<<"#########################################################"<<endl;
+        cout<<"choose your operation:";
+        if(!cinInt(sel) || sel < 0 || sel > 8){
+            cout<<"invalid input, please choose in [0-8]!"<<endl;
+            continue;
+        }
+
+        switch(sel){
+            case 1:{
+                if(!host_data.empty()){
+		    lock_s.lock();
+		    cout<<"No\t"<<"time"<<setw(13)<<"\t"<<"ip"<<setw(7)<<"\t"<<"pid\t"<<"type\t"<<"value\t"<<"comments"<<endl;
+		    for(int i=0;i<host_data.size();i++){
+			cout<<host_data[i].id<<"\t"<<host_data[i].time<<"\t"<<host_data[i].ip<<"\t"<<host_data[i].pid<<"\t"<<host_data[i].type<<"\t";
+			cout<<host_data[i].value.substr(0,6);//printf("%.*s",6,host_data[i].value.c_str());
+			cout<<"\t"<<host_data[i].comments<<"["<<host_data[i].comm_id<<"]"<<endl;
+		    }
+		    lock_s.unlock();
+		} else {
+		    cout<<"No data !"<<endl;
+		}
+                break;
+            }
+            case 2:{
+                if(!host_data.empty()){
+		    lock_s.lock();
+		    vector<int> temp;
+		    cout<<"No\t"<<"time"<<setw(13)<<"\t"<<"ip"<<setw(7)<<"\t"<<"pid\t"<<"type\t"<<"value\t"<<"comments"<<endl;
+		    for(int i=0;i<host_data.size();i++){
+			if(0==host_data[i].comm_id){
+			    temp.push_back(atoi(host_data[i].value.c_str()));
+			    cout<<host_data[i].id<<"\t"<<host_data[i].time<<"\t"<<host_data[i].ip<<"\t"<<host_data[i].pid<<"\t"<<host_data[i].type<<"\t";
+			    cout<<host_data[i].value.substr(0,6);
+			    cout<<"\t"<<host_data[i].comments<<"["<<host_data[i].comm_id<<"]"<<endl;
+			}
+		    }
+		    if(!temp.empty()){
+			float sum=accumulate(begin(temp),end(temp),0);
+			float mean=sum/temp.size();
+			cout<<"accumulate all the elements : "<<sum<<endl;
+			cout<<"mean of all the elements : "<<mean<<endl;
+			cout<<"max of all the elements : "<<*max_element(temp.begin(),temp.end())<<endl;
+			cout<<"min of all the elements : "<<*min_element(temp.begin(),temp.end())<<endl;
+		    } else {
+			cout<<"No such type data !"<<endl;
+		    }
+		    lock_s.unlock();
+		} else {
+		    cout<<"No data !"<<endl;
+		}
+                break;
+            }
+            case 3:{
+                if(!host_data.empty()){
+		    lock_s.lock();
+		    vector<int> temp;
+		    cout<<"No\t"<<"time"<<setw(13)<<"\t"<<"ip"<<setw(7)<<"\t"<<"pid\t"<<"type\t"<<"value\t"<<"comments"<<endl;
+		    for(int i=0;i<host_data.size();i++){
+			if(1==host_data[i].comm_id){
+			    temp.push_back(atoi(host_data[i].value.c_str()));
+			    cout<<host_data[i].id<<"\t"<<host_data[i].time<<"\t"<<host_data[i].ip<<"\t"<<host_data[i].pid<<"\t"<<host_data[i].type<<"\t";
+			    cout<<host_data[i].value.substr(0,6);
+			    cout<<"\t"<<host_data[i].comments<<"["<<host_data[i].comm_id<<"]"<<endl;
+			}
+		    }
+		    if(!temp.empty()){
+			float sum=accumulate(begin(temp),end(temp),0);
+			float mean=sum/temp.size();
+			cout<<"accumulate all the elements : "<<sum<<endl;
+			cout<<"mean of all the elements : "<<mean<<endl;
+			cout<<"max of all the elements : "<<*max_element(temp.begin(),temp.end())<<endl;
+			cout<<"min of all the elements : "<<*min_element(temp.begin(),temp.end())<<endl;
+		    } else {
+			cout<<"No such type data !"<<endl;
+		    }
+		    lock_s.unlock();
+		} else {
+		    cout<<"No data !"<<endl;
+		}
+                break;
+            }
+            case 4:{
+                if(!host_data.empty()){
+		    lock_s.lock();
+		    vector<int> temp;
+		    cout<<"No\t"<<"time"<<setw(13)<<"\t"<<"ip"<<setw(7)<<"\t"<<"pid\t"<<"type\t"<<"value\t"<<"comments"<<endl;
+		    for(int i=0;i<host_data.size();i++){
+			if(2==host_data[i].comm_id){
+			    temp.push_back(atoi(host_data[i].value.c_str()));
+			    cout<<host_data[i].id<<"\t"<<host_data[i].time<<"\t"<<host_data[i].ip<<"\t"<<host_data[i].pid<<"\t"<<host_data[i].type<<"\t";
+			    cout<<host_data[i].value.substr(0,6);
+			    cout<<"\t"<<host_data[i].comments<<"["<<host_data[i].comm_id<<"]"<<endl;
+			}
+		    }
+		    if(!temp.empty()){
+			float sum=accumulate(begin(temp),end(temp),0);
+			float mean=sum/temp.size();
+			cout<<"accumulate all the elements : "<<sum<<endl;
+			cout<<"mean of all the elements : "<<mean<<endl;
+			cout<<"max of all the elements : "<<*max_element(temp.begin(),temp.end())<<endl;
+			cout<<"min of all the elements : "<<*min_element(temp.begin(),temp.end())<<endl;
+		    } else {
+			cout<<"No such type data !"<<endl;
+		    }
+		    lock_s.unlock();
+		} else {
+		    cout<<"No data !"<<endl;
+		}
+                break;
+            }
+            case 5:{
+                if(!host_data.empty()){
+		    lock_s.lock();
+		    vector<float> temp;
+		    cout<<"No\t"<<"time"<<setw(13)<<"\t"<<"ip"<<setw(7)<<"\t"<<"pid\t"<<"type\t"<<"value\t"<<"comments"<<endl;
+		    for(int i=0;i<host_data.size();i++){
+			if(3==host_data[i].comm_id){
+			    temp.push_back(atof(host_data[i].value.c_str()));
+			    cout<<host_data[i].id<<"\t"<<host_data[i].time<<"\t"<<host_data[i].ip<<"\t"<<host_data[i].pid<<"\t"<<host_data[i].type<<"\t";
+			    cout<<host_data[i].value.substr(0,6);
+			    cout<<"\t"<<host_data[i].comments<<"["<<host_data[i].comm_id<<"]"<<endl;
+			}
+		    }
+		    if(!temp.empty()){
+			float sum=accumulate(begin(temp),end(temp),0.0);
+			float mean=sum/temp.size();
+			cout<<"accumulate all the elements : "<<sum<<endl;
+			cout<<"mean of all the elements : "<<mean<<endl;
+			cout<<"max of all the elements : "<<*max_element(temp.begin(),temp.end())<<endl;
+			cout<<"min of all the elements : "<<*min_element(temp.begin(),temp.end())<<endl;
+		    } else {
+			cout<<"No such type data !"<<endl;
+		    }
+		    lock_s.unlock();
+		} else {
+		    cout<<"No data !"<<endl;
+		}
+                break;
+            }
+            case 6:{
+                if(!host_data.empty()){
+		    lock_s.lock();
+		    vector<float> temp;
+		    cout<<"No\t"<<"time"<<setw(13)<<"\t"<<"ip"<<setw(7)<<"\t"<<"pid\t"<<"type\t"<<"value\t"<<"comments"<<endl;
+		    for(int i=0;i<host_data.size();i++){
+			if(4==host_data[i].comm_id){
+			    temp.push_back(atof(host_data[i].value.c_str()));
+			    cout<<host_data[i].id<<"\t"<<host_data[i].time<<"\t"<<host_data[i].ip<<"\t"<<host_data[i].pid<<"\t"<<host_data[i].type<<"\t";
+			    cout<<host_data[i].value.substr(0,6);
+			    cout<<"\t"<<host_data[i].comments<<"["<<host_data[i].comm_id<<"]"<<endl;
+			}
+		    }
+		    if(!temp.empty()){
+			float sum=accumulate(begin(temp),end(temp),0.0);
+			float mean=sum/temp.size();
+			cout<<"accumulate all the elements : "<<sum<<endl;
+			cout<<"mean of all the elements : "<<mean<<endl;
+			cout<<"max of all the elements : "<<*max_element(temp.begin(),temp.end())<<endl;
+			cout<<"min of all the elements : "<<*min_element(temp.begin(),temp.end())<<endl;
+		    } else {
+			cout<<"No such type data !"<<endl;
+		    }
+		    lock_s.unlock();
+		} else {
+		    cout<<"No data !"<<endl;
+		}
+                break;
+            }
+            case 7:{
+                if(!host_data.empty()){
+		    lock_s.lock();
+		    vector<float> temp;
+		    cout<<"No\t"<<"time"<<setw(13)<<"\t"<<"ip"<<setw(7)<<"\t"<<"pid\t"<<"type\t"<<"value\t"<<"comments"<<endl;
+		    for(int i=0;i<host_data.size();i++){
+			if(5==host_data[i].comm_id){
+			    temp.push_back(atof(host_data[i].value.c_str()));
+			    cout<<host_data[i].id<<"\t"<<host_data[i].time<<"\t"<<host_data[i].ip<<"\t"<<host_data[i].pid<<"\t"<<host_data[i].type<<"\t";
+			    cout<<host_data[i].value.substr(0,6);
+			    cout<<"\t"<<host_data[i].comments<<"["<<host_data[i].comm_id<<"]"<<endl;
+			}
+		    }
+		    if(!temp.empty()){
+			float sum=accumulate(begin(temp),end(temp),0.0);
+			float mean=sum/temp.size();
+			cout<<"accumulate all the elements : "<<sum<<endl;
+			cout<<"mean of all the elements : "<<mean<<endl;
+			cout<<"max of all the elements : "<<*max_element(temp.begin(),temp.end())<<endl;
+			cout<<"min of all the elements : "<<*min_element(temp.begin(),temp.end())<<endl;
+		    } else {
+			cout<<"No such type data !"<<endl;
+		    }
+		    lock_s.unlock();
+		} else {
+		    cout<<"No data !"<<endl;
+		}
+                break;
+            }
+            case 8:{
+                lock_s.lock();
+		host_data.clear();
+                lock_s.unlock();
+                break;
+            }
+            default: break;
+        }
+        sleep(1);
+    }
+    exit(0);
 }
--- host_v0.1/host/src/host.cpp
+++ host_v0.1/host/src/host.cpp
@@ -1,44 +1,39 @@
+/* user main(), need "embedded.cpp"
+* API : init_dic(), int-string(key-value) to describe the defination of data
+* API : process(int/float,int), save data to the vector, the first param is data, the second param is the dictionary key standing for data comments
+* API : host_show(), query data interactivily
+*/
 #include<iostream>
 #include<iomanip>
 #include<boost/thread/thread.hpp>
 #include<boost/bind.hpp>
 #include"embedded.cpp"
 using namespace std;
-void process_run(int interval){
+
+void process_run(int interval){  //thread for simulating user data
     int a=10;
     float b=5.6;
-    double c=7.9;
-    string str="dg952";
+    //double c=7.9;
+    //string str="dg952";
     while(true){
-	process(a++);
-	process(b++);
-	process(c++);
-	process(str);
-	sleep(interval);
-    }
-}
-void host_show(){
-    if(!host_data.empty()){
-	lock_s.lock();
-	cout<<"No\t"<<"time"<<setw(13)<<"\t"<<"ip"<<setw(7)<<"\t"<<"pid\t"<<"type\t"<<"value"<<endl;
-	for(int i=0;i<host_data.size();i++){
-	    cout<<host_data[i].id<<"\t"<<host_data[i].time<<"\t"<<host_data[i].ip<<"\t"<<host_data[i].pid<<"\t"<<host_data[i].type<<"\t"<<host_data[i].value<<endl;
-	}
-	lock_s.unlock();
-    }
-}
-void show_run(int interval){
-    while(true){
-	host_show();
-	cout<<endl;
+	//API to store data, according to the dictionary
+	process(a++,0);                     //0: robot_num(int)
+	process(rand()%100,1);              //1: packets(int)
+	process(rand()%1000,2);             //2: data_lenth(int)
+	process(b++,3);                     //3: speed(float)
+	process((float)(rand()%100+0.1),4); //4: signal(float)
+	process((float)(rand()%100+0.3),5); //5: error_rate(float)
+	//process(c++);
+	//process(str);
 	sleep(interval);
     }
 }
 int main(){
+    init_dic();  //init map<int,string> dic_comments, extendable, for example, 0:"robot_num", 1:"packets", 2:"data_lenth", 3:"speed", 4:"signal", 5:"error_rate"
     boost::thread_group thrd_grp;
-    int show_interval=0.02;
+    int show_interval=2;
     thrd_grp.create_thread(boost::bind(*process_run,show_interval));
-    thrd_grp.create_thread(boost::bind(*show_run,show_interval));
+    host_show(); //interact in terminal
     thrd_grp.join_all();
     return 0;
 }
