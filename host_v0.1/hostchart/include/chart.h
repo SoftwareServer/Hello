e3033fc2460dd87ba0972b11ac73ea00b6080747
SoftwareServer <yhlk2018@163.com>
2020-05-15 16:52:19 +0800

    Chart显示图表v0.3.3
    更新内容：
    1、修改hostchart结构体host
    2、修改dataSave同4部分host
    3、增加“Reset”（重置）、“Pause/Continue”（暂停/继续）、“Save”（保存）按钮

--- host_v0.1/hostchart/include/chart.h
+++ host_v0.1/hostchart/include/chart.h
@@ -6,7 +6,10 @@
 #include<qt5/QtCharts/QChart>
 #include<qt5/QtCharts/QLineSeries>
 #include<qt5/QtCharts/QScatterSeries>
+#include<QScreen>
 #include<QHBoxLayout>
+#include<QPushButton>
+#include<QFileDialog>
 #include<qt5/QtCharts/QBarSet>
 #include<qt5/QtCharts/QBarCategoryAxis>
 #include<qt5/QtCharts/QPercentBarSeries>
@@ -18,12 +21,11 @@ QT_CHARTS_USE_NAMESPACE
 
 struct host{            //similar to database
     std::string id;     //unique id for every row
-    std::string time;
-    std::string ip;     //local ip address
-    std::string pid;    //process number
-    std::string type;   //data type, such as int, float, double, string
+    std::string nodeId;
+    std::string timetag;
+    std::string attribute;
     std::string value;
-    std::string comments;
+    std::string notes;
 };
 
 class Chart:public QMainWindow{
@@ -33,34 +35,46 @@ public:
     Chart(QWidget *parent=nullptr,std::string lineTitle="null",std::string barTitle="null");
     ~Chart();
     void timeToDate(struct timeval tv,char *time);
-    void dataSave(int n,std::string comm);          //save data into vector<host>, as well as the same name method below
-    void dataSave(float n,std::string comm);
+    void dataSave(int n,std::string attribute,std::string notes="");          //save data into vector<host>, as well as the same name method below
+    void dataSave(float n,std::string attribute,std::string note="");
     void getData(int interval);   //save every type data periodly by SIGNAL 'timerUpdate'
 
 private slots:
     void update();                //SLOT for 'timerUpdate', update vector<host> by process()
     void result();                //SLOT for 'timer', update chart for display
+    void onBtnClicked1();
+    void onBtnClicked2();
+    bool onBtnClicked3();
+    void onBtnClicked4();
 
 private:
-    QChart *chartline;
-    QChart *chartbar;
-    QChartView *chartViewline;
-    QChartView *chartViewbar;
+    QChart *chartline=NULL;
+    QChart *chartbar=NULL;
+    QChartView *chartViewline=NULL;
+    QChartView *chartViewbar=NULL;
     QLineSeries *lineSeries=NULL;
     QScatterSeries *scatterSeries=NULL;
     QHBoxLayout *layout=NULL;
+    QHBoxLayout *vlayout=NULL;
     QGridLayout *baseLayout=NULL;
 
     QBarSet *set0 = NULL;
     QBarSet *set1 = NULL;
     QPercentBarSeries *series=NULL;
 
+    QPushButton *btn1=NULL;
+    QPushButton *btn2=NULL;
+    QPushButton *btn3=NULL;
+    QPushButton *btn4=NULL;
+    bool pause=false;
+    bool saveFile(const QString &fileName);
+
     QTimer timer;
     QTimer timerUpdate;
-    QList<float> data; // store data's list
-    float sig;
-    QList<int> dataInt; // store data's list
-    int sigInt;
+    QList<float> lineData; // store data's list
+    float barData;
+    QList<int> lineDataInt; // store data's list
+    int barDataInt;
     std::string lineTitle;
     std::string barTitle;
     std::vector<host> host_data;
--- host_v0.1/hostchart/src/chart.cpp
+++ host_v0.1/hostchart/src/chart.cpp
@@ -46,26 +46,44 @@ Chart::Chart(QWidget *parent,std::string lineTitle,std::string barTitle): QMainW
     chartViewbar->setRenderHint(QPainter::Antialiasing);
 
     layout = new QHBoxLayout();
-    layout->setContentsMargins(0, 0, 0, 0);
+    layout->setContentsMargins(2, 2, 2, 2);
 
     layout->addWidget(chartViewline);
     layout->addWidget(chartViewbar);
     layout->setStretchFactor(chartViewline,4);
     layout->setStretchFactor(chartViewbar,1);
 
+    btn1=new QPushButton("Reset");
+    btn2=new QPushButton("Pause");
+    btn3=new QPushButton("Save");
+    btn4=new QPushButton("Close");
+    vlayout = new QHBoxLayout();
+    vlayout->addWidget(btn1);
+    vlayout->addWidget(btn2);
+    vlayout->addWidget(btn3);
+    vlayout->addWidget(btn4);
+
     QWidget *pCentralWidget = new QWidget(this);
 
     baseLayout = new QGridLayout(pCentralWidget);
-    baseLayout->addLayout(layout, 0, 0, 0, 0);
+    //baseLayout->addLayout(layout, 0, 0, 0, 0);
+    //baseLayout->addLayout(vlayout, 0, 0, 0, 0);
+    baseLayout->addLayout(vlayout, 0, 0);
+    baseLayout->addLayout(layout, 1, 0);
+    //baseLayout->addLayout(vlayout, 1, 0);
 
     setCentralWidget(pCentralWidget);
 
     connect(&timer, SIGNAL(timeout()), this, SLOT(result()));
+    connect(btn1,SIGNAL(clicked()), this, SLOT(onBtnClicked1()));
+    connect(btn2,SIGNAL(clicked()), this, SLOT(onBtnClicked2()));
+    connect(btn3,SIGNAL(clicked()), this, SLOT(onBtnClicked3()));
+    connect(btn4,SIGNAL(clicked()), this, SLOT(onBtnClicked4()));
     timer.start(2 * 1000);
     a=0;
     b=1.1;
-    sig=0.0;
-    sigInt=0.0;
+    barData=0.0;
+    barDataInt=0.0;
 }
 Chart::~Chart(){
     this->close();
@@ -97,6 +115,10 @@ Chart::~Chart(){
 	delete layout;
 	layout=NULL;
     }
+    if(NULL!=vlayout){
+	delete vlayout;
+	vlayout=NULL;
+    }
     if(NULL!=baseLayout){
 	delete baseLayout;
 	baseLayout=NULL;
@@ -109,13 +131,29 @@ Chart::~Chart(){
 	delete set1;
 	set1=NULL;
     }
+    if(NULL!=btn1){
+	delete btn1;
+	btn1=NULL;
+    }
+    if(NULL!=btn2){
+	delete btn2;
+	btn2=NULL;
+    }
+    if(NULL!=btn3){
+	delete btn3;
+	btn3=NULL;
+    }
+    if(NULL!=btn4){
+	delete btn4;
+	btn4=NULL;
+    }
 }
 void Chart::update(){
     dataSave(++a,"robot num");
     dataSave(rand()%100,"packets");
     dataSave(rand()%1000,"data length");
     dataSave(++b,"speed");
-    dataSave((float)(rand()%100+0.1),"signal");
+    dataSave((float)(rand()%100+0.1),"signal","percent");
     dataSave((float)(rand()%100+0.3),"error rate");
     //qDebug()<<host_data.size();
 }
@@ -123,25 +161,25 @@ void Chart::result(){
     if(dic.find(this->lineTitle)!=dic.end()){
 	//qDebug()<<"find "<<this->lineTitle.c_str();
 	// more than max, delete the early data
-	while (data.size() > 31) {
-	    data.removeFirst();
+	while (lineData.size() > 31) {
+	    lineData.removeFirst();
 	}
 	// hide
 	float max=0;
-	if(!data.empty()){
-	    max=data.at(0);
+	if(!lineData.empty()){
+	    max=lineData.at(0);
 	}
 	if(isVisible()){
 	    lineSeries->clear();
 	    scatterSeries->clear();
 	    int dx = 30 / (31-1);
-	    int less = 31 - data.size();
-	    for (int i = 0; i < data.size(); ++i) {
-		if(data.at(i)>max){
-		    max=data.at(i);
+	    int less = 31 - lineData.size();
+	    for (int i = 0; i < lineData.size(); ++i) {
+		if(lineData.at(i)>max){
+		    max=lineData.at(i);
 		}
-		lineSeries->append(less*dx+i*dx, data.at(i));
-		scatterSeries->append(less*dx+i*dx, data.at(i));
+		lineSeries->append(less*dx+i*dx, lineData.at(i));
+		scatterSeries->append(less*dx+i*dx, lineData.at(i));
 	    }
 	}
 	if(100<max){
@@ -154,25 +192,25 @@ void Chart::result(){
     else if(dicInt.find(this->lineTitle)!=dicInt.end()){
 	//qDebug()<<"find "<<this->lineTitle.c_str();
 	// more than max, delete the early data
-	while (dataInt.size() > 31) {
-	    dataInt.removeFirst();
+	while (lineDataInt.size() > 31) {
+	    lineDataInt.removeFirst();
 	}
 	// hide
 	int max=0;
-	if(!data.empty()){
-	    max=data.at(0);
+	if(!lineDataInt.empty()){
+	    max=lineDataInt.at(0);
 	}
 	if(isVisible()){
 	    lineSeries->clear();
 	    scatterSeries->clear();
 	    int dx = 30 / (31-1);
-	    int less = 31 - dataInt.size();
-	    for (int i = 0; i < dataInt.size(); ++i) {
-		if(dataInt.at(i)>max){
-		    max=dataInt.at(i);
+	    int less = 31 - lineDataInt.size();
+	    for (int i = 0; i < lineDataInt.size(); ++i) {
+		if(lineDataInt.at(i)>max){
+		    max=lineDataInt.at(i);
 		}
-		lineSeries->append(less*dx+i*dx, dataInt.at(i));
-		scatterSeries->append(less*dx+i*dx, dataInt.at(i));
+		lineSeries->append(less*dx+i*dx, lineDataInt.at(i));
+		scatterSeries->append(less*dx+i*dx, lineDataInt.at(i));
 	    }
 	}
 	if(100<max){
@@ -191,17 +229,17 @@ void Chart::result(){
     if(dic.find(this->barTitle)!=dic.end()){
 	//qDebug()<<"find "<<this->barTitle.c_str();
 	if(isVisible()){
-	    set0->replace(0,sig);
-	    set1->replace(0,100.0-sig);
-	    //qDebug()<<"sig : "<<sig;
+	    set0->replace(0,barData);
+	    set1->replace(0,100.0-barData);
+	    //qDebug()<<"barData : "<<barData;
 	}
     }
     else if(dicInt.find(this->barTitle)!=dicInt.end()){
 	//qDebug()<<"find "<<this->barTitle.c_str();
 	if(isVisible()){
-	    set0->replace(0,sigInt);
-	    set1->replace(0,100.0-sigInt);
-	    //qDebug()<<"sigInt : "<<sigInt;
+	    set0->replace(0,barDataInt);
+	    set1->replace(0,100.0-barDataInt);
+	    //qDebug()<<"barDataInt : "<<barDataInt;
 	}
     }
     else{
@@ -215,7 +253,7 @@ void Chart::timeToDate(struct timeval tv,char *time){
     localtime_r(&tv.tv_sec,&tv_temp);
     snprintf(time,25,"%04d-%02d-%02d %02d:%02d:%02d.%03ld",tv_temp.tm_year+1900,tv_temp.tm_mon+1,tv_temp.tm_mday,tv_temp.tm_hour,tv_temp.tm_min,tv_temp.tm_sec,tv.tv_usec/1000);
 }
-void Chart::dataSave(int n,std::string comm){
+void Chart::dataSave(int n,std::string attribute,std::string notes){
     char ip_num[20] = "";
     get_local_ip("eth0",ip_num);    //local network name, such as 'eth0', 'enpXsX' and so on
     struct timeval nowTus;
@@ -224,24 +262,23 @@ void Chart::dataSave(int n,std::string comm){
     timeToDate(nowTus,time);
     host temp;
     temp.id=to_string(host_data.size()+1);
-    temp.time=time;
-    temp.ip=ip_num;
-    temp.pid=to_string(getpid());
-    temp.type="int";
+    temp.nodeId=ip_num;
+    temp.timetag=time;
+    temp.attribute=attribute;
     temp.value=to_string(n);
-    temp.comments=comm;
+    temp.notes=notes;
     lock_s.lock();
-    dicInt.insert(comm);
-    if(this->lineTitle==comm){
-	dataInt<<n;
+    dicInt.insert(attribute);
+    if(this->lineTitle==attribute){
+	lineDataInt<<n;
     }
-    if(this->barTitle==comm){
-	sigInt=n;
+    if(this->barTitle==attribute){
+	barDataInt=n;
     }
     host_data.push_back(temp);       //save data to "database", vector<host>
     lock_s.unlock();
 }
-void Chart::dataSave(float n,std::string comm){
+void Chart::dataSave(float n,std::string attribute,std::string notes){
     char ip_num[20] = "";
     get_local_ip("eth0",ip_num);    //local network name, such as 'eth0', 'enpXsX' and so on
     struct timeval nowTus;
@@ -250,19 +287,18 @@ void Chart::dataSave(float n,std::string comm){
     timeToDate(nowTus,time);
     host temp;
     temp.id=to_string(host_data.size()+1);
-    temp.time=time;
-    temp.ip=ip_num;
-    temp.pid=to_string(getpid());
-    temp.type="float";
+    temp.nodeId=ip_num;
+    temp.timetag=time;
+    temp.attribute=attribute;
     temp.value=to_string(n);
-    temp.comments=comm;
+    temp.notes=notes;
     lock_s.lock();
-    dic.insert(comm);
-    if(this->lineTitle==comm){
-	data<<n;
+    dic.insert(attribute);
+    if(this->lineTitle==attribute){
+	lineData<<n;
     }
-    if(this->barTitle==comm){
-	sig=n;
+    if(this->barTitle==attribute){
+	barData=n;
     }
     host_data.push_back(temp);
     lock_s.unlock();
@@ -270,4 +306,88 @@ void Chart::dataSave(float n,std::string comm){
 void Chart::getData(int interval){
     connect(&timerUpdate, SIGNAL(timeout()), this, SLOT(update()));
     timerUpdate.start(interval * 1000);
+}
+bool Chart::saveFile(const QString &fileName){
+    if(!chartViewline){
+	return false;
+    }
+    if(!chartViewbar){
+	return false;
+    }
+    QPixmap map(chartViewline->width()+chartViewbar->width(),chartViewline->height());
+    QPixmap pline;
+    QPixmap pbar;
+    pline=this->chartViewline->grab();
+    pbar=this->chartViewbar->grab();
+    QPainter p(&map);
+    p.drawPixmap(0,0,pline);
+    p.drawPixmap(pline.width(),0,pbar);
+    QImage image=map.toImage();
+    if(fileName.endsWith(".png")){
+	image.save(fileName.mid(0,fileName.indexOf("."))+".png");
+    }else{
+	image.save(fileName+".png");
+    }
+    return true;
+}
+void Chart::onBtnClicked1(){
+    if(!host_data.empty()){
+	lock_s.lock();
+	host_data.clear();
+	lock_s.unlock();
+    }
+    if(!dic.empty()){
+	lock_s.lock();
+	dic.clear();
+	lock_s.unlock();
+    }
+    if(!dicInt.empty()){
+	lock_s.lock();
+	dicInt.clear();
+	lock_s.unlock();
+    }
+    if(!lineData.empty()){
+	lock_s.lock();
+	lineData.clear();
+	lock_s.unlock();
+    }
+    if(!lineDataInt.empty()){
+	lock_s.lock();
+	lineDataInt.clear();
+	lock_s.unlock();
+    }
+    lineSeries->clear();
+    scatterSeries->clear();
+    set0->replace(0,0);
+    set1->replace(0,1);
+    timer.start();
+    if(pause){
+	btn2->setText("Pause");
+	pause=false;
+    }
+}
+void Chart::onBtnClicked2(){
+    if(pause){
+	timer.start();
+	btn2->setText("Pause");
+	pause=false;
+	//qDebug()<<"continue";
+    }else{
+	timer.stop();
+	btn2->setText("Continue");
+	pause=true;
+	//qDebug()<<"pause";
+    }
+}
+bool Chart::onBtnClicked3(){
+    QString fileName = QFileDialog::getSaveFileName(this,
+                               tr("Save Charts"), ".",
+                               tr("PNG files (*.png)"));
+    if (fileName.isEmpty()){
+        return false;
+    }
+    return saveFile(fileName);
+}
+void Chart::onBtnClicked4(){
+    this->close();
 }
